b7bc241c232acc8f30dc09b9d3c8c8574b2c7070
Calle <calle.agelii01@gmail.com>
2023-10-03 15:37:59 +0200

    small change

--- MA4_Files/MA4_2.py
+++ MA4_Files/MA4_2.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3.9
 
 from person import Person
+from numba import njit
+from time import perf_counter as timer
+import matplotlib.pyplot as plt
+
+def fib_py(n):
+	if n <= 1:
+		return n
+	else:
+		return(fib_py(n-1) + fib_py(n-2))
+	
+@njit
+def fib_numba(n):
+	if n <= 1:
+		return n
+	else:
+		return(fib_py(n-1) + fib_py(n-2))
 
 def main():
 	f = Person(5)
